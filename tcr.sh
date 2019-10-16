11909291b21c16d2c5b4d4148dbc55fc5190a286
Michael Whatcott <mdwhatcott@gmail.com>
2019-10-15 18:35:03 -0600

    The script.

---
+++ tcr.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# TCR: test && commit || revert
+# codified by Kent Beck
+# https://medium.com/@kentbeck_7670/test-commit-revert-870bbd756864
+
+function test() {
+    echo
+    cd `git rev-parse --show-toplevel`
+    go fmt ./...
+    go test ./...
+}
+function commit() {
+    echo
+    git add . 
+    git commit -m "tcr"
+}
+function revert() {
+    echo
+    git reset --hard
+}
+
+test && commit || revert
