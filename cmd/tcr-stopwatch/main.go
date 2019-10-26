d4b506b1aaf34b19820f8328c80a5a01c67a71ad
Michael Whatcott <mdwhatcott@gmail.com>
2019-10-25 19:29:21 -0600

    Server-side rendering of recorded times as an ordered list.

--- cmd/tcr-stopwatch/main.go
+++ cmd/tcr-stopwatch/main.go
@@ -1,39 +1,64 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os/exec"
+	"strings"
 	"sync"
 	"time"
 )
 
-func main() {
-	var (
-		mutex   sync.Mutex
-		started = time.Now()
-	)
+var (
+	mutex   sync.Mutex
+	started = time.Now()
+	times   []time.Duration
+)
 
+func main() {
 	router := http.NewServeMux()
 	router.HandleFunc("/stopwatch/reset", func(http.ResponseWriter, *http.Request) {
 		mutex.Lock()
+		times = append(times, since(started))
 		started = time.Now()
 		mutex.Unlock()
 	})
 	router.HandleFunc("/stopwatch", func(response http.ResponseWriter, _ *http.Request) {
 		mutex.Lock()
-		io.WriteString(response, time.Since(started).Round(time.Second).String())
+		io.WriteString(response, renderDurations(times))
 		mutex.Unlock()
 	})
 	router.HandleFunc("/", func(response http.ResponseWriter, _ *http.Request) {
 		io.WriteString(response, uiHTML)
 	})
 
-	address := ":7890"
+	address := "localhost:7890"
 	log.Printf("[INFO] Listening for web traffic on %s.", address)
+	go func() { _ = exec.Command("open", "http://"+address).Run() }()
 	if err := http.ListenAndServe(address, router); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func since(then time.Time) time.Duration {
+	return time.Since(then).Round(time.Second)
+}
+
+func renderDuration(duration time.Duration) string {
+	return fmt.Sprintf("<li>%s</li>\n", duration.String())
+}
+
+func renderDurations(durations []time.Duration) string {
+	var builder strings.Builder
+	builder.WriteString("<ol>\n")
+	builder.WriteString(renderDuration(since(started)))
+	for x := len(durations) - 1; x >= 0; x-- {
+		builder.WriteString(renderDuration(durations[x]))
+	}
+	builder.WriteString("</ol>")
+	return builder.String()
 }
 
 const uiHTML = `
