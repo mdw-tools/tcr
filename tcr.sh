50fa34c5b2dd528e771a5a4868f8a456ee311cff
Michael Whatcott <mdwhatcott@gmail.com>
2019-10-31 13:55:14 -0600

    Including tcr commit count in report. Dressed up the report.

--- tcr.sh
+++ tcr.sh
@@ -6,23 +6,47 @@
 
 function test() {
     echo
+    echo '/////////////'
+    echo '// TESTING //'
+    echo '/////////////'
+    echo
     curl "http://localhost:7890/stopwatch/reset" &>/dev/null # reset timer (if running)
     cd `git rev-parse --show-toplevel` # navigate to top-level of git repo
     make tcr
-    # TODO: echo how many tcr commits we currently have...
 }
 function commit() {
     echo
+    echo '////////////////'
+    echo '// COMMITTING //'
+    echo '////////////////'
+    echo
     git add .
     git commit -m "tcr"
+    echo
+    printf 'TCR commit count: %s\n' `git log --oneline origin/master..HEAD | grep tcr | wc -l | tr -d '[:space:]'`
+    echo
+    echo "////////"
+    echo "// OK //"
+    echo "////////"
     return 0
 }
 function revert() {
+    echo
+    echo '///////////////'
+    echo '// REVERTING //'
+    echo '///////////////'
     echo
     echo "less is more" | tee >(pbcopy)
     echo
     git clean -df
     git reset --hard
+    echo
+    echo "///////////"
+    echo "// ERROR //"
+    echo "///////////"
+    return 1
 }
 
 test && commit || revert
+
+
