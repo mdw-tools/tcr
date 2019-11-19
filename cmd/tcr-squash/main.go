81fd76d63d904f993234a92a1e45e6045cc4390d
Michael Whatcott <mdwhatcott@gmail.com>
2019-11-19 15:08:10 -0700

    Implemented squash script.

--- cmd/tcr-squash/main.go
+++ cmd/tcr-squash/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/mdwhatcott/tcr.sh/exec"
+)
+
+func main() {
+	count := exec.GetTCRCommitCount()
+	fmt.Printf("Squashing [%d] commits into single, staged change set and opening smerge...\n", count)
+	fmt.Println(exec.RunOrFatal("", "git", "reset", "--soft", fmt.Sprintf("HEAD~%d", count)))
+	_, _ = exec.Run("", "smerge", ".")
 }
