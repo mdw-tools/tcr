92ede7c959d51a555c66eaf667020c7d937e22e7
Michael Whatcott <mdwhatcott@gmail.com>
2020-05-30 13:03:39 -0600

    Don't start sublime merge.
    Latest version of sublime merge goes into safe mode on startup.

--- cmd/tcr-squash/main.go
+++ cmd/tcr-squash/main.go
@@ -10,10 +10,4 @@ func main() {
 	count := exec.GetTCRCommitCount()
 	fmt.Printf("Squashing [%d] commits into single, staged change set and opening smerge...\n", count)
 	fmt.Println(exec.RunFatal("git", exec.Args("reset", "--soft", fmt.Sprintf("HEAD~%d", count))))
-	output, err := exec.Run("smerge", exec.Args("."))
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-	fmt.Println("Output:")
-	fmt.Println(output)
 }
